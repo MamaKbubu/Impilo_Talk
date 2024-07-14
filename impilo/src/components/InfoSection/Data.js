7577f5b5082f8d43b8e3124ad8413706ceb17690
MamaKbubu <maboelatk@gmail.com>
2024-07-14 19:56:37 +0200

    files

---
+++ impilo/src/components/InfoSection/Data.js
@@ -0,0 +1,52 @@
+export const homeObjOne = {
+  id: "about",
+  lightBg: false,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: "About Your Safety",
+  headline: "",
+  description:
+    "Personal safety is paramount when you find yourself in a dangerous place. That's why being a SAFE Agent will benefit you. As SAFE, we make sure that you are connected.Our SOS alert system ensures your safety by working seamlessly with your phone or smartwatch. Once activated, our application immediately notifies our network of SAFE agents and local authorities, ensuring a rapid response to any emergency. Stay connected and feel secure knowing help is always just a tap away, providing you with peace of mind wherever you go. ",
+  buttonLabel: "Get Started",
+  imgStart: false,
+  img: require("../../images/svg-1.svg"),
+  //   alt: "Car",
+  dark: true,
+  primary: true,
+  darkText: false,
+};
+
+export const homeObjTwo = {
+  id: "discover",
+  lightBg: true,
+  lightText: true,
+  lightTextDesc: false,
+
+  headline: "Hints on how to stay safe",
+  description:
+    "1. Be aware of your surroundings. 2. Keep your phone charged. 3. Trust your instincts. 4. Avoid isolated areas. 5. Be a member with SAFE, become an agent so you are visible to other agents.",
+  buttonLabel: "SIGN UP???",
+  //   imgStart: true,
+  //   img: require("../../images/svg-3.svg").default,
+  //   alt: require("../../images/svg-3.svg").default,
+  dark: false,
+  primary: false,
+  darkText: true,
+};
+// export const homeObjThree = {
+//   id: "signup",
+//   lightBg: false,
+//   lightText: false,
+//   lightTextDesc: true,
+//   topLine: "sh1t",
+//   headline: "Lets get your puppy started :):):)",
+//   description:
+//     "Get access to our exclusive app that allows you to send unlimited transactions without getting charged any fees.",
+//   buttonLabel: "Get started",
+//   imgStart: false,
+//   img: require("../../images/svg-3.svg"),
+//   alt: "Car",
+//   dark: false,
+//   primary: false,
+//   darkText: true,
+// };
