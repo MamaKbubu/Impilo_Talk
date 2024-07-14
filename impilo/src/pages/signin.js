8f2222f740bfd720f1d300b6043a7b27f7c4263b
MamaKbubu <maboelatk@gmail.com>
2024-07-14 20:11:04 +0200

    pages

---
+++ impilo/src/pages/signin.js
@@ -0,0 +1,13 @@
+import React from "react";
+import SignIn from "../components/SignIn";
+// import ScollToTop from "../components/SignIn/ScrollToTop";
+
+const SignInPage = () => {
+  return (
+    <>
+      <SignIn />
+    </>
+  );
+};
+
+export default SignInPage;
