f05b810eb4e1f846d77596c6a344e73c5be41b0e
MamaKbubu <maboelatk@gmail.com>
2024-07-14 18:36:05 +0200

    Footer

---
+++ impilo/src/components/Footer/index.js
@@ -0,0 +1,76 @@
+import React from "react";
+import {
+  FooterContainer,
+  FooterWrap,
+  FooterLinksContainer,
+  FooterLinksWrapper,
+  FooterLinkItems,
+  FooterLinkTitle,
+  FooterLink,
+  SocialMedia,
+  SocialMediaWrap,
+  SocialLogo,
+  WebsiteRights,
+  SocialIcons,
+  SocialIconLink,
+} from "./FooterElements";
+import { FaFacebook, FaInstagram, FaWhatsapp } from "react-icons/fa";
+
+const Footer = () => {
+  return (
+    <>
+      <FooterContainer>
+        <FooterWrap>
+          <FooterLinksContainer>
+            <FooterLinksWrapper>
+              <FooterLinkItems>
+                <FooterLinkTitle>About Us </FooterLinkTitle>
+                <FooterLink to="/about">About Us</FooterLink>
+                <FooterLink to="/testimonials">Testimonials</FooterLink>
+                <FooterLink to="/terms">Terms of Service</FooterLink>
+              </FooterLinkItems>
+              <FooterLinkItems>
+                <FooterLinkTitle>Contact Us </FooterLinkTitle>
+                <FooterLink to="/contact">Contact Us</FooterLink>
+                <FooterLink to="/support">Support</FooterLink>
+              </FooterLinkItems>
+            </FooterLinksWrapper>
+          </FooterLinksContainer>
+          <SocialMedia>
+            <SocialMediaWrap>
+              <SocialLogo to="/">THE MOB</SocialLogo>
+              <WebsiteRights>
+                The_Mob © {new Date().getFullYear()} All rights reserved.
+              </WebsiteRights>
+              <SocialIcons>
+                <SocialIconLink
+                  href="https://www.facebook.com/profile.php?id=100087758115687"
+                  target="_blank"
+                  aria-label="Facebook"
+                >
+                  <FaFacebook />
+                </SocialIconLink>
+                <SocialIconLink
+                  href="https://www.instagram.com/doggycloud_sa/"
+                  target="_blank"
+                  aria-label="Instagram"
+                >
+                  <FaInstagram />
+                </SocialIconLink>
+                <SocialIconLink
+                  href="https://wa.me/+27676904536"
+                  target="_blank"
+                  aria-label="Twitter"
+                >
+                  <FaWhatsapp />
+                </SocialIconLink>
+              </SocialIcons>
+            </SocialMediaWrap>
+          </SocialMedia>
+        </FooterWrap>
+      </FooterContainer>
+    </>
+  );
+};
+
+export default Footer;
